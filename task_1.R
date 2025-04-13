d623fff38e092a61c12770ca13e5d2eac35d49f5
kaushik911 <kaushikshamantha@gmail.com>
2025-04-14 01:14:10 +0400

    improve the charts

--- task_1.R
+++ task_1.R
@@ -1,11 +1,9 @@
-# Task 1
+candy_data = read.csv("CMM703 Candy Data 2024-2025.csv", header = TRUE)
 
-# Chart 1
-
-candy_data = read.csv("CMM703 Candy Data 2024-2025.csv")
-
+# these are the candy ingredients
 ingredients = c("chocolate", "fruity", "caramel", "peanutyalmondy", "nougat", "crispedricewafer", "hard", "bar", "pluribus")
 
+# lets analysis favorite candy ingredient
 feature_average = data.frame(
                     feature_name = character(),
                     average_win_percentage = numeric()
@@ -13,6 +11,8 @@ feature_average = data.frame(
 
 for (ingredient in ingredients) {
   has_feature     = candy_data[candy_data[[ingredient]] == 1, ]
+  
+  # calculate the average 'winpercent' where particular ingredient is in the candy
   average_win     = mean(has_feature$winpercent, na.rm = TRUE)
   feature_average = rbind(
                       feature_average,
@@ -22,6 +22,18 @@ for (ingredient in ingredients) {
                       )
                     )
 }
+
+barplot(
+  feature_average$average_win_percentage,
+  names.arg = feature_average$feature_name,
+  las  = 2,
+  main = "Voters' Favourite Candy Ingredients",
+  xlab = "Ingredient",
+  ylab = "Average Win Percent (%)",
+)
+
+# as you can see here it is bit hard to analysis the information given in the chart.
+# lets improve the chart by adding colors, sort the data and few extra labels.
 
 # sort by popularity
 feature_average = feature_average[order(-feature_average$average_win_percentage), ]
@@ -50,16 +62,32 @@ text(
   pos = 3
 )
 
-abline(h = 50, col = "blue", lty = 2)  # red dashed line at 50%
+# blue dashed line at 50%
+abline(h = 50, col = "blue", lty = 2)
 
-# chart 2
+# according to the chart you can see that most popular candy ingredient is "crispedricewafer" and the least is candy which is "hard".
+# there are 5 candy ingredients achieve more that 50%
+# 'chocolate' and 'nougat' ingredients almost share the same popularity
 
+# lets pick 4 ingredients where the most popular
 candy_data$candy_type = "other"
 candy_data$candy_type[candy_data$crispedricewafer == 1] = "crispedricewafer"
 candy_data$candy_type[candy_data$peanutyalmondy == 1]   = "peanutyalmondy"
 candy_data$candy_type[candy_data$bar == 1]              = "bar"
 
 candy_data$sugarpercent = candy_data$sugarpercent * 100
+
+plot(
+  x = candy_data$sugarpercent,
+  y = candy_data$winpercent,
+  main = "Sugar Content vs Popularity",
+  xlab = "Sugar Percentage",
+  ylab = "Win Percentage (Popularity)",
+)
+
+# this scatter plot shows how candy distribute across win percentage against sugar percentage.
+# as you can see it is not easy to get information from the chart.
+# lets improve the chart by adding colors, some info replated to the price.
 
 par(mar = c(4, 4, 4, 2))
 
@@ -105,3 +133,7 @@ legend(
   pt.cex = 2,
   horiz  = TRUE,
 )
+
+# here in the chart you can see that data is scatter between (20 - 85)% in the y axis the win percentage
+# according to the chart more popular candys have higher sugar content.
+# popular and sugary candys are not cheep according to the chart
--- task_2.R
+++ task_2.R
@@ -1,52 +1,54 @@
-original_bank_data = read.csv("Bank Churn Data CMM703.csv")
-colnames(original_bank_data)
-dim(original_bank_data)
+original_bank_data = read.csv("Bank Churn Data CMM703.csv", header = TRUE)
 
-head(original_bank_data, 10)
+# number of columns/variables
+ncol(original_bank_data)
+#columns names
+colnames(original_bank_data)
+
+# number of row
+nrow(original_bank_data)
+
+# quick overview of the dataset
+str(original_bank_data)
+
 # by looking at the dataset description its seems that HasCrCard, IsActiveMember, and Exited are categorical variables
 # lets convert them into categorical variables using factors
 
-# customer id and surname is not required for the analysis
+# before that, customer id and surname is not required for the analysis, so remove it from the dataset
 original_bank_data$CustomerId = NULL
 original_bank_data$Surname    = NULL
 
-# remove the null values if exists
+# lets look at null values in the dataset
+sapply(
+  original_bank_data,
+  FUN = function(x) sum(is.na(x))
+)
+
+# remove the null values if exists, since there are no null values
 bank_data = na.omit(original_bank_data)
 
 # variable delete because not used from here(had to remove because need memory for modeling)
 original_bank_data = NULL
 
-# change the data type as factor
-bank_data$HasCrCard = factor(
-  bank_data$HasCrCard,
-  levels = c(0, 1),
-  labels = c("No", "Yes")
-)
+change_to_factor = function(param_bank_data, param_feature) {
+  return(
+    factor(
+      param_bank_data[[param_feature]],
+      levels = c(0, 1),
+      labels = c("No", "Yes")
+    )
+  )
+}
 
-bank_data$IsActiveMember = factor(
-  bank_data$IsActiveMember,
-  levels = c(0, 1),
-  labels = c("No", "Yes")
-)
-
-bank_data$Exited = factor(
-  bank_data$Exited,
-  levels = c(0, 1),
-  labels = c("No", "Yes")
-)
+bank_data$HasCrCard = change_to_factor(bank_data, "HasCrCard")
+bank_data$IsActiveMember = change_to_factor(bank_data, "IsActiveMember")
+bank_data$Exited = change_to_factor(bank_data, "Exited")
 
 # lets do a quick summary statistics
-
 # i install "vtable" package because it seems more eye pleasing
 # install.packages('vtable')
 library(vtable)
 sumtable(bank_data)
-
-counts = table(bank_data$Exited)
-percentages = round(counts / sum(counts) * 100, 2)
-labels = paste(names(counts), percentages, "%")
-colors = c("green", "red")
-pie(counts, labels = labels, col = colors, main = "Churn(Exited) status of customers")
 
 # quantitative variables
 featureset = c("Age", "CreditScore", "Balance", "EstimatedSalary", "Tenure")
@@ -54,14 +56,14 @@ featureset = c("Age", "CreditScore", "Balance", "EstimatedSalary", "Tenure")
 exited_customers = subset(bank_data, Exited == "Yes")
 stayed_customers = subset(bank_data, Exited == "No")
 
-plot_histogram = function (param_feature, param_bank_data) {
+plot_histogram = function (param_feature, param_bank_data, param_title) {
 
   column_data = param_bank_data[[param_feature]]
 
   hist(
     column_data,
-    main = paste("Histogram of", param_feature),
-    col = "lightblue",
+    main = paste("Histogram of", param_feature, param_title),
+    col = "gray",
     breaks = 10
   )
 
@@ -87,7 +89,7 @@ plot_boxplot = function(param_feature, param_bank_data) {
   original_data = param_bank_data[[param_feature]]
 
   # split the data by 'Exited' and convert it to a dataframe
-  grouped_data = split(original_data, param_bank_data[['Exited']])
+  grouped_data = split(original_data, param_bank_data[["Exited"]])
 
   # create a list including all data + grouped data
   final_data = c(list(all_data = original_data), grouped_data)
@@ -108,9 +110,9 @@ for(feature in featureset) {
   layout(matrix(1:4, ncol = 1, byrow = TRUE))
   par(mar = c(3, 3, 2, 1))  # reduce margins
 
-  plot_histogram(feature, bank_data)
-  plot_histogram(feature, exited_customers)
-  plot_histogram(feature, stayed_customers)
+  plot_histogram(feature, bank_data, "")
+  plot_histogram(feature, exited_customers, "(Exited Customers)")
+  plot_histogram(feature, stayed_customers, "(Stayed Customers)")
   
   plot_boxplot(feature, bank_data)
 }
@@ -119,27 +121,29 @@ for(feature in featureset) {
 
 featureset = c("Geography", "Gender", "NumOfProducts", "HasCrCard", "IsActiveMember")
 
-plot_bargraph = function (param_feature, param_bank_data) {
+plot_bargraph = function (param_feature, param_bank_data, param_title) {
 
-  feature_count = table(param_bank_data[[param_feature]])
-  
+  feature_data  = param_bank_data[[param_feature]]
+  feature_count = sort(table(feature_data), decreasing = TRUE)
+
+  bar_colors = terrain.colors(length(names(feature_count)))
+
   barplot(
     feature_count,
-    col = "lightblue",
-    main = paste(param_feature),
+    col = bar_colors,
+    main = paste("Bar Graph of", param_feature, param_title),
     ylim = c(0, max(feature_count) + 10)
   )
 }
-
 
 for(feature in featureset) {
 
   layout(matrix(1:3, ncol = 1, byrow = TRUE))
   par(mar = c(3, 3, 2, 1))  # reduce margins
 
-  plot_bargraph(feature, bank_data)
-  plot_bargraph(feature, exited_customers)
-  plot_bargraph(feature, stayed_customers)
+  plot_bargraph(feature, bank_data, "")
+  plot_bargraph(feature, exited_customers, "(Exited Customers)")
+  plot_bargraph(feature, stayed_customers, "(Stayed Customers)")
 }
 
 exited_customers = NULL
@@ -151,7 +155,17 @@ stayed_customers = NULL
 # model should be binary logistic regression model
 
 
-# lets analysis the data in the dataset
+counts = table(bank_data$Exited)
+print(counts)
+# you can see the percentages are not equal meaning dataset is not balance
+
+percentages = round(counts / sum(counts) * 100, 2)
+labels = paste(names(counts), percentages, "%")
+colors = terrain.colors(2)
+par(mfrow = c(1, 1))
+pie(counts, labels = labels, col = colors, main = "Churn(Exited) status of customers")
+
+# lets start create a model to check customer churn status
 set.seed(2425499)
 
 number_of_rows   = nrow(bank_data)
@@ -160,8 +174,6 @@ train_ids = sample(1:number_of_rows, number_of_rows * train_percentage, replace 
 
 train_dataset = bank_data[train_ids, ]
 test_dataset  = bank_data[-train_ids, ]
-
-names(bank_data)
 
 bank_model = glm(
   formula = Exited ~ .,
@@ -197,80 +209,33 @@ table(
 table(test_dataset$Exited)
 
 mean(test_dataset$predicted_class == test_dataset$Exited)
+# accuracy is 81.25% (0.8125)
 
 # goodness of fit of the model 
 # hosmer and lemeshow test 
 # h0 : model is adequate
 # h1 : model is not adequate 
 
-install.packages("modEvA")
+# install.packages("modEvA")
 library(modEvA)
 
 hoslem_results <- HLfit(
-  model = refined_bank_model,  # Your logistic regression model
-  bin.method = "n.bins",      # Group observations into bins
-  n.bins = 10,                # Number of bins (typically 10-15)
-  main = "Model Goodness-of-Fit Check (10 Bins)"  # Plot title
+  model = refined_bank_model,
+  bin.method = "n.bins",
+  n.bins = 10,
+  main = "Model Goodness-of-Fit Check (10 Bins)"
 )
 
 # Print the test results
+par(mfrow = c(1, 1))
 print(hoslem_results)
 
+# p-value is 0.038 < 0.05 meaning we reject h0.
+# the conclusion of the statement is model is the model is not adequate.
 
+# model has 81.25% accuracy, but the Hosmer-Lemeshow (H-L) test shows p-value is 0.038
+# that means a potential lack of fit.
 
-
-
-# forward selection for a better model
-# lets start from with an empty model and then adding parameters one by one based on significance (p-values)
-
-# first the null model with the intercept(c)
-
-null_model = glm(
-  formula = Exited ~ 1,  # only the intercept
-  data = train_dataset,
-  family = binomial
-)
-
-all_model_variables = c( "CreditScore", "Geography", "Gender", "Age",  "Tenure", "Balance", "NumOfProducts", "HasCrCard", "IsActiveMember", "EstimatedSalary" )
-
-current_model = null_model
-
-remaining_model_variables = all_model_variables
-selected_model_variables  = c()
-
-threshold_p_value = 0.05  # significance level
-
-# iterate until no significant predictors left
-while (length(remaining_model_variables) > 0) {
-
-  p_values = c()
-
-  for (predictor in remaining_predictors) {
-
-    temp_formula = as.formula(
-      paste("Exited ~", paste(c(selected_predictors, predictor), collapse = " + "))
-    )
-
-    temp_model = glm(
-      formula = temp_formula,
-      data = train_dataset,
-      family = binomial
-    )
-
-    # get p-values
-    p_value  = coef(summary(temp_model))[predictor, "Pr(>|z|)"]
-    p_values = c(p_values, p_value)
-  }
-
-  min_p_value = min(p_values)
-  if (min_p_value > threshold_p_value) break
-
-  
-}
-
-
-
-
-
-
-
+# what is the next step then,
+# * we can change the weights meaning 'glm' function support 'weights' as a parameter.
+# * we can give priority to the 'Yes' and redo the modeling.
